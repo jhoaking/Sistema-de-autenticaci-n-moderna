6cca17f4d9172c849814efc64eb22fc59f66cdc0
jhoaking <peredoroj@gmail.com>
2025-08-01 13:26:57 -0400

    conexion a base de datos mas entidad de auth hecha mas contenedor de docker funcionando

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {PassportModule} from '@nestjs/passport'
+import {ConfigModule, ConfigService} from '@nestjs/config'
+import {JwtModule} from '@nestjs/jwt'
+
+
+import { Auth } from './entities/auth.entity';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports : [
+    ConfigModule, 
+    TypeOrmModule.forFeature([Auth]),
+    PassportModule.register({defaultStrategy : 'jwt'}),
+
+    JwtModule.registerAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      useFactory : (configService:ConfigService)=>{
+        return {
+          secret : configService.get('JWT_SECRET'),
+          signOptions:{
+            expiresIn : '2h'
+          }
+        }
+      }
+    })
+  ],
+  exports : [TypeOrmModule,PassportModule,JwtModule]
 })
 export class AuthModule {}
--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,31 @@
-export class Auth {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({name : 'users'})
+export class Auth {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  fullName: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text', {
+    select: true,
+  })
+  password: string;
+
+  @Column('bool', {
+    default: true,
+  })
+  isActive: boolean;
+
+  @Column('text', {
+    array: true,
+    default: ['user'],
+  })
+  roles: string[];
+}
